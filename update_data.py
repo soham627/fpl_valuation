0fc0f601233f9d0e5c964ce8e639156798b9bbe8
Soham Mall <malls@uchicago.edu>
2024-08-18 11:46:16 +0530

    first data download of 24-25 season

--- update_data.py
+++ update_data.py
@@ -178,8 +178,8 @@ con = sqlalchemy.create_engine(uri, encoding='utf8')
 ## removing the foreign key constraints after the tables already exist so that they can be dropped and replaced
 
 ## COMMENT THIS BACK IN AFTER FIRST RUN 
-con.execute('alter table record drop constraint teamer')
-con.execute('alter table record drop constraint player_connect')
+#con.execute('alter table record drop constraint teamer')
+#con.execute('alter table record drop constraint player_connect')
 
 ### saving tables from previous season - COMMENT THIS OUT AFTER FIRST RUN
 #con.execute('create table a_player_2324 AS TABLE player')
